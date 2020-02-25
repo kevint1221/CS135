944cd205df296d95ab3ea47de63543f259f757c8
kevint1221 <32470783+kevint1221@users.noreply.github.com>
2020-02-25 04:00:40 -0800

    added global variable

---
+++ Final_project/global_variable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class global_variable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool hit_sound_enable;
+
+    void Start()
+    {
+        hit_sound_enable = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
