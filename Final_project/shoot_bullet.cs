5ea5055f6511be27c2518b87f7d70be3d36bdcfb
kevint1221 <32470783+kevint1221@users.noreply.github.com>
2020-02-25 04:06:32 -0800

    add explosion sound

--- Final_project/shoot_bullet.cs
+++ Final_project/shoot_bullet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +8,17 @@ public class shoot_bullet : MonoBehaviour
     public GameObject hitbox;
     public GameObject front_end;
     public GameObject back_end;
+    public AudioSource shoot_source; //audio object
+    public AudioClip shoot_clip;//audio effect
+    public AudioSource hit_source;
+    public AudioClip hit_clip;
+    
     Vector3 bullet_position;
     Vector3 offset;
+    Vector3 explosion_location;
     float right_index;
     float counter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,17 +36,26 @@ public class shoot_bullet : MonoBehaviour
             //if(OVRInput.GetDown(OVRInput.RawButton.A))
             {
                 offset = front_end.transform.position - back_end.transform.position;
-                bullet_position = hitbox.transform.position + new Vector3(0f, 0f, 3f);
+                explosion_location = 4 * offset;
+                bullet_position = hitbox.transform.position;// + new Vector3(0f, 0f, 3f);
                 GameObject bullet = Instantiate(bullet_object, bullet_position, Quaternion.identity);
                 bullet.GetComponent<Rigidbody>().velocity = 100f * offset;
-                
+                //AudioSource hit_audio = Instantiate(asource, explosion_location, Quaternion.identity);
                 counter = 0.5f;
+                //asource.transform.position = explosion_location;
+                shoot_source.PlayOneShot(shoot_clip); //play sound
                 Destroy(bullet, 2f);
+                
             }
             
         }
         counter -= Time.deltaTime;
-        
+        if(global_variable.hit_sound_enable == true)
+        {
+            hit_source.PlayOneShot(hit_clip);
+            global_variable.hit_sound_enable = false;
+
+        }
         //bullet.transform.position += offset;
 
     }
