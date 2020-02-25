7794e8b0064f2e73aec1413e4485ab53d2ff6d59
kevint1221 <32470783+kevint1221@users.noreply.github.com>
2020-02-24 17:07:14 -0800

    added audio

--- Final_project/Bullet_Effect.cs
+++ Final_project/Bullet_Effect.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet_Effect : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject bullet;
-    //public GameObject aim_scope;
+    public GameObject bullet; //sphere in this case
     public float damage =10f;
     public float range = 100f;
-    public GameObject impact_effect;
-    public GameObject impact_effect2;
-    Vector3 effect_area;
+    public GameObject impact_effect; //flare
+    public GameObject impact_effect2; //smoke
+    public AudioSource asource; //audio object
+	public AudioClip aclip;//audio effect
+    
+    Vector3 effect_area; //position to make impact_effect and audio effect
 
     void OnTriggerEnter(Collider other)
     {
@@ -36,7 +38,15 @@ public class Bullet_Effect : MonoBehaviour
         //destroy hit effect after 2second
         Destroy(impact_object, 4f);
         Destroy(impact_object2, 4f);
+
+        //if the bullet hit the object, make explode sound
+        asource.transform.position = effect_area
+        asource.PlayOneShot(aclip); //play sound
     }
 
     
 }
+
+
+
+
