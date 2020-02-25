3f7ba5095bf438800b369be2838df9d19f1ebc9b
kevint1221 <32470783+kevint1221@users.noreply.github.com>
2020-02-25 04:03:51 -0800

    add hit sound
    when hit_sound_enable play hit sound

--- Final_project/Bullet_Effect.cs
+++ Final_project/Bullet_Effect.cs
@@ -6,27 +6,27 @@ public class Bullet_Effect : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject bullet; //sphere in this case
-    public float damage =10f;
+    public float damage = 10f;
     public float range = 100f;
     public GameObject impact_effect; //flare
     public GameObject impact_effect2; //smoke
     public AudioSource asource; //audio object
     public AudioClip aclip;//audio effect
-    
+
     Vector3 effect_area; //position to make impact_effect and audio effect
 
     void OnTriggerEnter(Collider other)
     {
-        Shoot();
+        Hit_object();
     }
 
 
-    void Shoot()
+    void Hit_object()
     {
         //hitted object
         RaycastHit hit;
         //if hit the object
-        if(Physics.Raycast(bullet.transform.position, bullet.transform.forward, out hit, range))
+        if (Physics.Raycast(bullet.transform.position, bullet.transform.forward, out hit, range))
         {
             //Display object name
             Debug.Log(hit.transform.name);
@@ -35,18 +35,15 @@ public class Bullet_Effect : MonoBehaviour
         //create hit effect
         GameObject impact_object = Instantiate(impact_effect, effect_area, Quaternion.LookRotation(hit.normal));
         GameObject impact_object2 = Instantiate(impact_effect2, effect_area, Quaternion.LookRotation(hit.normal));
+        //if the bullet hit the object, make explode sound
+        global_variable.hit_sound_enable = true;
         //destroy hit effect after 2second
         Destroy(impact_object, 4f);
         Destroy(impact_object2, 4f);
 
-        //if the bullet hit the object, make explode sound
-        asource.transform.position = effect_area
-        asource.PlayOneShot(aclip); //play sound
+        
+       
     }
 
-    
+
 }
-
-
-
-
